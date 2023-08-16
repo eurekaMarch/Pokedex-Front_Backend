7d6c236814ddc2e85d95fcad481fe7d0ac178b18
Trairat Kaewsaad <121665180+eurekaMarch@users.noreply.github.com>
2023-08-16 19:22:51 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { Button } from "antd";
+import { Button, Image } from "antd";
+import PokeTitle from "./assets/images/pokedex.png";
 
 function App() {
   return (
     <div>
       <div>App</div>
+      <Image width={200} src={PokeTitle} />
       <Button type="primary">Clickme</Button>
     </div>
   );
